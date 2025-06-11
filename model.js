0ea15265ff48d6583235354866a4ec916d5274cf
Gabrielle De Castro Santos Catarino <catarinogabrielle@gmail.com>
2025-06-11 17:50:17 -0300

    at67

--- model.js
+++ model.js
@@ -1,17 +1,8 @@
 import { GoogleGenerativeAI } from '@google/generative-ai';
 
-const API_KEY = 'AIzaSyB-6aiH7pqNGk7C9QsjkUc8rcg95wEs5dU';
-
-let genAI;
-if (API_KEY) {
-  genAI = new GoogleGenerativeAI(API_KEY);
-} else {
-  console.error("A variável de ambiente GEMINI_API_KEY não foi definida.");
-}
-
-export async function initializeModel(modelName) {
-  if (!genAI) {
-    throw new Error("GoogleGenerativeAI não foi inicializado. Verifique a chave da API.");
-  }
-  return genAI.getGenerativeModel({ model: modelName });
+export async function initializeModel(model) {
+  const apiKey = "AIzaSyB-6aiH7pqNGk7C9QsjkUc8rcg95wEs5dU";
+  const genAI = new GoogleGenerativeAI(apiKey);
+  const modelAI = genAI.getGenerativeModel({ model: model });
+  return modelAI;
 }
